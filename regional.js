ac410d98a3f692a1c03a7fcfafd6a8ee31ce991f
brandoncano2 <brandonleon1208@gmail.com>
2023-06-09 21:58:35 -0500

    actualización pagina

--- regional.js
+++ regional.js
@@ -1,124 +1,77 @@
-
 var preguntasRespuestas = [
+  {
+    pregunta: "hola",
+    respuesta: "Hola, Mi nombre es Brandon Asistente Virtual."
+  },
+  {
+    pregunta: "ayuda",
+    respuesta: "Claro, En qué puedo servirte?"
+  },
+  {
+    pregunta: "duracion",
+    respuesta: "El tour tiene una duración de 2 días."
+  },
+  {
+    pregunta: "costo",
+    respuesta: "El tour tiene un costo de $300.000 pesos por persona."
+  },
+  {
+    pregunta: "incluye",
+    respuesta: "El tour incluye hospedaje, guianza, recorrido, alimentación y seguro de vida."
+  },
 
-   
-    {
-      pregunta: "hola",
-      respuesta: "Hola, Mi nombre es Brandon Asistente Virtual."
-    },
+  {
+    pregunta: "formas",
+    respuesta: "Las formas de pago que puedes utilizar son efectivo(CUOTAS), tarjeta de credito y transferencia."
+  },
 
-    {
-        pregunta: "Hola",
-        respuesta: "Hola, Mi nombre es Brandon Asistente Virtual."
-      },
-
-    {
-      pregunta: "como estas",
-      respuesta: "Estoy bien, ¡gracias por preguntar!"
-    },
-
-    {
-        pregunta: "como estas?",
-        respuesta: "Estoy bien, ¡gracias por preguntar!"
-      },
-
-    {
-      pregunta: "me ayudas?",
-      respuesta: "En que puedo servirte?"
-    },
-{
-        pregunta: "me ayudas",
-        respuesta: "En que puedo servirte?"
-      },
-    {
-        pregunta: "Me ayudas?",
-        respuesta: "En que puedo servirte?"
-      },
-    // Agrega más preguntas y respuestas aquí
-    {
-        pregunta: "cuanto dura el tour",
-        respuesta: "El tour tiene una duración de 2 días."
-      },
-
-    {
-        pregunta: "cuanto dura el tour?",
-        respuesta: "El tour tiene una duración de 2 días."
-      },
-
-    {
-        pregunta: "cual es la duración del tour?",
-        respuesta: "El tour tiene una duración de 2 días."
-      },
-
-    {
-        pregunta: "cuanto cuesta el tour?",
-        respuesta: "El tour tiene un costo de $300.000 pesos colombianos por persona."
-      },
-
-      {
-        pregunta: "cual es el costo del tour?",
-        respuesta: "El tour tiene un costo de $300.000 pesos colombianos por persona."
-      },
-
-      {
-        pregunta: "cuantas cuotas debo pagar?",
-        respuesta: "Puedes pagar el tour con un plazo de 3 cuotas, una la pagas este mes de junio, la siguiente en el mes de julio y la ultima para finalizar tu pago en el mes de agosto."
-      },
-
-      {
-        pregunta: "Formas de pago?",
-        respuesta: "Puedes pagar el tour a cuotas de x plata por mes."
-      },
-    {
-        pregunta: "que incluye el tour?",
-        respuesta: "El tour incluye Hospedaje, Guianza, Recorrido, Alimentación, Seguro de vida."
-      },
-
-  ];
-
-  function enviarMensaje() {
-    var userInput = document.getElementById("user-input");
-    var chatContainer = document.getElementById("chat-container");
-
-    var userMessage = document.createElement("p");
-    userMessage.innerText = "Usuario: " + userInput.value;
-    chatContainer.appendChild(userMessage);
-
-    obtenerRespuesta(userInput.value);
-    userInput.value = "";
-    chatContainer.scrollTop = chatContainer.scrollHeight;
+  {
+    pregunta: "cuotas",
+    respuesta: "Debes de pagar 3 cuotas en total. 1-En el mes de junio 2- En Julio 3- Esta ultima se paga en Agosto."
   }
+];
 
-  function obtenerRespuesta(pregunta) {
-    var respuestaEncontrada = false;
+function enviarMensaje() {
+  var userInput = document.getElementById("user-input");
+  var chatContainer = document.getElementById("chat-container");
 
-    for (var i = 0; i < preguntasRespuestas.length; i++) {
-      var pr = preguntasRespuestas[i];
+  var selectedOption = userInput.value;
+  var selectedQuestion = userInput.options[userInput.selectedIndex].text;
 
-      if (pr.pregunta.toLowerCase() === pregunta.toLowerCase()) {
-        mostrarRespuesta(pr.respuesta);
-        respuestaEncontrada = true;
-        break;
-      }
-    }
+  var userMessage = document.createElement("p");
+  userMessage.innerText = "Usuario: " + selectedQuestion;
+  chatContainer.appendChild(userMessage);
 
-    if (!respuestaEncontrada) {
-      mostrarRespuesta("Lo siento, no tengo respuesta para esa pregunta.");
+  obtenerRespuesta(selectedOption);
+  userInput.value = "";
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+}
+
+function obtenerRespuesta(pregunta) {
+  var respuestaEncontrada = false;
+
+  for (var i = 0; i < preguntasRespuestas.length; i++) {
+    var pr = preguntasRespuestas[i];
+
+    if (pr.pregunta === pregunta) {
+      mostrarRespuesta(pr.respuesta);
+      respuestaEncontrada = true;
+      break;
     }
   }
 
-  function mostrarRespuesta(respuesta) {
-    var chatContainer = document.getElementById("chat-container");
-
-    var assistantMessage = document.createElement("p");
-    assistantMessage.classList.add("message", "assistant-message");
-    assistantMessage.innerText = "Asistente: " + respuesta;
-    chatContainer.appendChild(assistantMessage);
-
-    chatContainer.scrollTop = chatContainer.scrollHeight;
+  if (!respuestaEncontrada) {
+    mostrarRespuesta("Lo siento, no tengo respuesta para esa pregunta.");
   }
+}
 
-document.getElementById("chat-container").addEventListener ("submit", function (event){
-    event.preventDefault();
-    enviarMensaje();
-});
+function mostrarRespuesta(respuesta) {
+  var chatContainer = document.getElementById("chat-container");
+
+  var assistantMessage = document.createElement("p");
+  assistantMessage.classList.add("message", "assistant-message");
+  assistantMessage.innerText = "Asistente: " + respuesta;
+  chatContainer.appendChild(assistantMessage);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+}
